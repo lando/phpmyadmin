fec62e768320ce4e99f4e6b555c3fcdea1a44965
Mike Pirog <mike@lando.dev>
2022-05-05 09:01:26 -0400

    Update lando/phpmyadmin to use @lando/vuepress-theme-default-plus@1.0.0-beta.32 (#6)
    * AUTODEPLOYED @lando/vuepress-theme-default-plus@1.0.0-beta.32
    
    * Update config.js
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: John Ouellet <john@thinktandem.io>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+const customTheme = require('@lando/vuepress-theme-default-plus');
 
 module.exports = {
   lang: 'en-US',
@@ -12,8 +13,7 @@ module.exports = {
     ['link', {rel: 'preconnect', href: '//fonts.gstatic.com', crossorigin: true}],
     ['link', {rel: 'stylesheet', href: '//fonts.googleapis.com/css2?family=Lexend:wght@500&display=swap'}],
   ],
-  theme: '@lando/vuepress-theme-default-plus',
-  themeConfig: {
+  theme: customTheme({
     landoDocs: true,
     logo: '/images/icon.svg',
     docsDir: 'docs',
@@ -45,5 +45,5 @@ module.exports = {
       {text: 'Release Notes', link: 'https://github.com/lando/phpmyadmin/releases'},
       '/development.html',
     ],
-  },
+  }),
 };
